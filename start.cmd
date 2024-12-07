cca626d0fcd28ec4ac3f0533a7897a24fda4dcc9
aasdvv <aasdvv@gmail.com>
2024-12-07 19:16:50 +0300

    .

--- start.cmd
+++ start.cmd
@@ -1,3 +1,3 @@
-resizeimage.exe -input "c:\Downloads\1111\Foto" -width 1920 -r -quality 70 -threads 8
+resizeimage.exe -input "\\NAS\phoneBackup\Laluka\LPhoto2024" -width 2312 -r -quality 60 -threads 1
 
 pause
